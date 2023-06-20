56b24bc0b0b46b884f3f1b354405b0d37920c4c4
Emanuele Biselli <emanuele.biselli@pegaso2000.it>
2023-06-20 12:47:37 +0200

    primo commit - creazione del progetto + config db

---
+++ src/test/java/it/pegaso2000/fastFoodApi/FastFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.pegaso2000.fastFoodApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
