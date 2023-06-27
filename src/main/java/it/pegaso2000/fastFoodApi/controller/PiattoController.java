484109631483cb66dff2d5b21cad15154caec3c7
Emanuele Biselli <emanuele.biselli@pegaso2000.it>
2023-06-27 11:50:11 +0200

    ho creato i vari controller con all, find, save e delete

--- src/main/java/it/pegaso2000/fastFoodApi/controller/PiattoController.java
+++ src/main/java/it/pegaso2000/fastFoodApi/controller/PiattoController.java
@@ -1,13 +1,27 @@
 package it.pegaso2000.fastFoodApi.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import it.pegaso2000.fastFoodApi.dto.PiattoDto;
 import it.pegaso2000.fastFoodApi.model.Piatto;
 import it.pegaso2000.fastFoodApi.service.PiattoService;
 
+@RestController
+@RequestMapping("api/piatto")
 public class PiattoController {
-	
+
 	private PiattoService service;
 
 	@Autowired
@@ -15,7 +29,37 @@ public class PiattoController {
 		super();
 		this.service = service;
 	}
-	
+
+	@GetMapping("/all")
+	public ResponseEntity<List<PiattoDto>> all() {
+		List<Piatto> list = service.findAll();
+		List<PiattoDto> result = list.stream().map(cliente -> {
+			return entityToDto(cliente);
+		}).collect(Collectors.toList());
+		return new ResponseEntity<List<PiattoDto>>(result, HttpStatus.OK);
+	}
+
+	@GetMapping("/findById/{id}")
+	public ResponseEntity<PiattoDto> findById(@PathVariable int id) {
+		Piatto c = service.findById(id);
+		if (c == null)
+			return new ResponseEntity<PiattoDto>(HttpStatus.OK);
+		PiattoDto dto = entityToDto(c);
+		return new ResponseEntity<PiattoDto>(dto, HttpStatus.OK);
+	}
+
+	@PostMapping("/save")
+	public ResponseEntity<PiattoDto> save(@RequestBody PiattoDto dto) {
+		Piatto saving = DtoToentity(dto);
+		Piatto saved = service.save(saving);
+		return new ResponseEntity<PiattoDto>(entityToDto(saved), HttpStatus.OK);
+	}
+
+	@DeleteMapping("delete/{id}")
+	public boolean delete(@PathVariable int id) {
+		return service.delete(id);
+	}
+
 	private PiattoDto entityToDto(Piatto p) {
 		PiattoDto dto = new PiattoDto();
 		dto.setId(p.getId());
@@ -23,5 +67,13 @@ public class PiattoController {
 		dto.setDescrizione(p.getDescrizione());
 		dto.setPrezzo(p.getPrezzo());
 		return dto;
+	}
+
+	private Piatto DtoToentity(PiattoDto dto) {
+		Piatto p = new Piatto();
+		p.setDescrizione(dto.getDescrizione());
+		p.setIs_vegetariano(dto.isIs_vegetariano());
+		p.setPrezzo(dto.getPrezzo());
+		return p;
 	}
 }
